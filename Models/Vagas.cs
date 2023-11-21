1c9bf327750dd9c5fa494f573a8d32d9877be885
Andreyna618 <81197697+Andreyna618@users.noreply.github.com>
2023-11-21 08:04:58 -0300

    Add files via upload

---
+++ Models/Vagas.cs
@@ -0,0 +1,9 @@
+public class Vagas
+{
+    public int id {get;set;}
+    public string cargo { get; set; }
+    public string modelo {get;set; }
+    public string tipo {get;set ;}
+    public string local { get; set; }
+    public string descricao { get; set; }
+}
